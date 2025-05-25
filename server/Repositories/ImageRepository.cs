b1024db90f1abf60a4368a9d93007562d0dd2dac
Ivan <mayhemk57@gmail.com>
2025-05-25 09:22:06 -0600

    image repo mostly built, need to add values to create and update

--- server/Repositories/ImageRepository.cs
+++ server/Repositories/ImageRepository.cs
@@ -7,4 +7,47 @@ public class ImageRepository : IRepository<Image>
   {
     _db = db;
   }
+
+  public IEnumerable<Image> GetAll()
+  {
+    return _db.Query<Image>("SELECT * FROM images;");
+  }
+
+  public Image GetById(int id)
+  {
+    return _db.QueryFirstOrDefault<Image>("SELECT * FROM images WHERE id = @Id;", new { id });
+  }
+
+  public Image Create(Image imageData)
+  {
+    string sql = @"INSERT INTO
+    images (stuff)
+    VALUES (@Stuff); 
+    
+    SELECT images.*,
+    accounts.*
+    FROM images
+    INNER JOIN accounts on accounts.id = images.creator_id
+    WHERE images.id = LAST_INSERT_ID();";
+
+    Image createdImage = _db.Query(sql, (Image image, Account account) =>
+    {
+      image.Creator = account;
+      return image;
+    }, imageData).SingleOrDefault();
+    return createdImage;
+  }
+
+  public bool Delete(int id)
+  {
+    string sql = "DELETE FROM images WHERE id = @Id LIMIT 1;";
+    return _db.Execute(sql, new { id }) > 0;
+  }
+
+  public Image Update(Image updateData)
+  {
+    string sql = "UPDATE images SET id = @Id;";
+    _db.Execute(sql, updateData);
+    return updateData;
+  }
 }
