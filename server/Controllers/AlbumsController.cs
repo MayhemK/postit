8ea65d9b6f673989694f8b3a886aa489448e8d00
Ivan <mayhemk57@gmail.com>
2025-05-27 16:36:10 -0600

    Implement album retrieval and archiving methods in AlbumsController; enhance error handling and response structure.

--- server/Controllers/AlbumsController.cs
+++ server/Controllers/AlbumsController.cs
@@ -26,5 +26,83 @@ public class AlbumsController(AlbumsService albumsService, Auth0Provider auth0Pr
     }
   }
 
+  [HttpGet]
+  public ActionResult<List<Album>> GetAlbums([FromQuery] string category)
+  {
+    try
+    {
+      List<Album> albums;
+      if (category == null)
+      {
+        albums = _albumsService.GetAlbums();
+      }
+      else
+      {
+        albums = _albumsService.GetAlbums(category);
+      }
+      return Ok(albums);
+    }
+    catch (Exception exception)
+    {
+      return BadRequest(exception.Message);
+    }
+  }
 
+  [HttpGet("{albumId}")]
+  public ActionResult<Album> GetAlbumById(int albumId)
+  {
+    try
+    {
+      Album album = _albumsService.GetAlbumById(albumId);
+      return Ok(album);
+    }
+    catch (Exception exception)
+    {
+      return BadRequest(exception.Message);
+    }
+  }
+
+  [Authorize]
+  [HttpDelete("{albumId}")]
+  public async Task<ActionResult<Album>> ArchiveAlbum(int albumId)
+  {
+    try
+    {
+      Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+      Album album = _albumsService.ArchiveAlbum(albumId, userInfo);
+      return Ok(album);
+    }
+    catch (Exception exception)
+    {
+      return BadRequest(exception.Message);
+    }
+  }
+
+  [HttpGet("{albumId}/pictures")]
+  public ActionResult<List<Image>> GetPicturesByAlbumId(int albumId)
+  {
+    try
+    {
+      List<Image> images = _imagesService.GetImageByAlbumId(albumId);
+      return Ok(images);
+    }
+    catch (Exception exception)
+    {
+      return BadRequest(exception.Message);
+    }
+  }
+
+  // [HttpGet("{albumId}/watchers")]
+  // public ActionResult<List<watcherProfile>> GetWatcherProfileByAlbumId(int albumId)
+  // {
+  //   try
+  //   {
+  //     List<WatcherProfile> watcherProfiles = _watcherService.GetWatcherProfilesByAlbumId(albumId);
+  //     return Ok(watcherProfiles);
+  //   }
+  //   catch (Exception exception)
+  //   {
+  //     return BadRequest(exception.Message);
+  //   }
+  // }
 }
