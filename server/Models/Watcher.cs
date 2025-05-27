cd814514b1b469812b05dfc568df5dacd1497de8
Ivan <mayhemk57@gmail.com>
2025-05-27 16:54:13 -0600

    Add Watcher, WatcherProfile, and WatcherAlbum models; implement WatcherRepository for database interactions

---
+++ server/Models/Watcher.cs
@@ -0,0 +1,22 @@
+namespace postit.Models;
+
+public class Watcher
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  public string AccountId { get; set; }
+  public int AlbumId { get; set; }
+}
+
+public class WatcherProfile : Account
+{
+  public int WatcherId { get; set; }
+  public int AlbumId { get; set; }
+}
+
+public class WatcherAlbum : Album
+{
+  public int WatcherId { get; set; }
+  public string AccountId { get; set; }
+}
